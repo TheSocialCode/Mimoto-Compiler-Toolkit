298c443a20fc85e2798787102a56136e90744d1c
Sebastian Kersten <sebastiankersten@gmail.com>
2024-09-27 14:02:51 +0200

    Added setup to init project

--- index.js
+++ index.js
@@ -15,63 +15,74 @@ const CloneFile = require('./src/CloneFile');
 
 
 
-// --- special functions
 
 
-// 3. clone watched file
-let bIsCloning = false;
-const args = process.argv.slice(2); // removes the first two default elements
-args.forEach((val, index) => { if (val === '-clone') { new CloneFile(args[index + 1], args[index + 2]); bIsCloning = true; } });
-if (bIsCloning) return;
+// Get the command (second argument)
+const sCommand = process.argv[0];
+
+// Get the full list of arguments
+const aArgs = process.argv.slice(3);
 
 
-
-// --- core functions
-
-
-
-// ADD - Promise
+// Check which command was passed and call the appropriate function
+switch (sCommand.toLowerCase()) {
+    case 'init':
 
 
-// 4. load configuration file mimoto.config.json
-const config = (() =>
-{
-    // a. get root directory
-    let sRootDir = '';
-    const args = process.argv.slice(2); // removes the first two default elements
-    args.forEach((val, index) => { if (val === '-root') { sRootDir = args[index + 1]; } });
-    const RUNTIME_ROOT = path.join(process.cwd(), sRootDir);
+        new InitProject();
 
-    // b. load
-    try
-    {
-        // I. point to the directory where the script is executed, not where it is located
-        const configFile = path.join(RUNTIME_ROOT, 'mimoto.config.json');
+        break;
+    case 'clone':
 
-        // II. load and convert
-        const config = JSON.parse(fs.readFileSync(configFile, 'utf8'));
+        // check if args 0 en 1 gezet en exist?
+        new CloneFile(aArgs[0], aArgs[1]);
 
-        // III. extend
-        config.RUNTIME_ROOT = RUNTIME_ROOT;
+        break;
+    case 'compile':
+    default:
 
-        // IV. send
-        return config;
+        // 4. load configuration file mimoto.config.json
+        const config = (() =>
+        {
+            // a. get root directory
+            let sRootDir = '';
+            const args = process.argv.slice(2); // removes the first two default elements
+            args.forEach((val, index) => { if (val === '-root') { sRootDir = args[index + 1]; } });
+            const RUNTIME_ROOT = path.join(process.cwd(), sRootDir);
 
-    } catch(error) {
+            // b. load
+            try
+            {
+                // I. point to the directory where the script is executed, not where it is located
+                const configFile = path.join(RUNTIME_ROOT, 'mimoto.config.json');
 
-        // I. report error
-        console.log('🚨 - WARNING - Missing config file \u001b[1m\u001B[31mmimoto.config.json\u001B[0m\u001b[22m in project root');
+                // II. load and convert
+                const config = JSON.parse(fs.readFileSync(configFile, 'utf8'));
 
-        // II. exit
-        process.exit(1);
-    }
-})();
+                // III. extend
+                config.RUNTIME_ROOT = RUNTIME_ROOT;
 
-// 5. distribute Mimoto.js
-const mimotoDistributor = new DistributeMimoto(config);
+                // IV. send
+                return config;
 
-// 6. distribute
-let distributor = mimotoDistributor.distribute();
+            } catch(error) {
 
-// 7. setup template combiner
-distributor.then(() => { const templateCombiner = new CombineTemplates(config); })
+                // I. report error
+                console.log('🚨 - WARNING - Missing config file \u001b[1m\u001B[31mmimoto.config.json\u001B[0m\u001b[22m in project root');
+
+                // II. exit
+                process.exit(1);
+            }
+        })();
+
+        // 5. distribute Mimoto.js
+        const mimotoDistributor = new DistributeMimoto(config);
+
+        // 6. distribute
+        let distributor = mimotoDistributor.distribute();
+
+        // 7. setup template combiner
+        distributor.then(() => { const templateCombiner = new CombineTemplates(config); })
+
+        break;
+}
---
+++ src/InitProject.js
@@ -0,0 +1,80 @@
+/**
+ * Mimoto Compiler Toolkit - A tiny helper for building Mimoto projects
+ * @author - Sebastian Kersten (sebastian@thesocialcode.com)
+ */
+
+
+// import core classes
+const fs = require('fs');
+const path = require('path');
+const shell = require('shelljs');
+
+// import Mimoto util classes
+const DataUtils = require("../toolkit/utils/DataUtils");
+
+
+class InitProject
+{
+
+	// data
+	_config = null;
+
+
+
+	// ----------------------------------------------------------------------------
+	// --- Constructor ------------------------------------------------------------
+	// ----------------------------------------------------------------------------
+
+
+	/**
+	 * Constructor
+	 * @param sFileToWatch
+	 * @param sDestinationFile
+	 */
+	constructor(sFileToWatch, sDestinationFile)
+	{
+		// 1. validate file to watch or exit
+		if (!fs.existsSync(sFileToWatch))
+		{
+			console.log('\n');
+			console.log('🚨 - WARNING - Clone function unable to find file \u001b[1m\u001B[31m' + sFileToWatch + '\u001B[0m\u001b[22m to watch');
+			process.exit(1);
+		}
+
+		// 2. validate destination path or exit
+		if (!sDestinationFile)
+		{
+			console.log('\n');
+			console.log('🚨 - WARNING - Clone function needs a destination path and file name');
+			process.exit(1);
+		}
+
+		// 3. watch for changes in the file
+		fs.watch(sFileToWatch, (sEventType, sFileName) =>
+		{
+			// a. validate or exit
+			if (!(sFileName && sEventType === 'change')) return;
+
+			// b. clone
+			this._cloneFile(sFileToWatch, sDestinationFile);
+		});
+
+		// 4. report
+		console.log(`Watching for file changes on ${sFileToWatch}\n`);
+
+		// 5. initial clone
+		this._cloneFile(sFileToWatch, sDestinationFile);
+	}
+
+
+
+	// ----------------------------------------------------------------------------
+	// --- Private methods --------------------------------------------------------
+	// ----------------------------------------------------------------------------
+
+
+
+
+}
+
+module.exports = InitProject;
